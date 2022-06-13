f8c685ee4137772efaf5d5a2bddfdad660792bd7
Luke Sturgill <lukesturgill@gmail.com>
2022-06-13 01:31:28 -0400

    wrote out service-worker.js and added comments to idb.js

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,21 +1,21 @@
-const { request } = require("express");
-
 const database;
 
-// Connect to IndexedDB
+// Connect to IndexedDB, set to version 1
 const connect = indexedDB.open('budget_tracker', 1);
 
 connect.onupgradeneeded = function(event) {
+    // save a reference to the database
     const database = event.target.result;
-
+    // create an object store (table) called 'new_transaction' set with auto incrementing primary keys
     database.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 connect.onsuccess = function(event) {
     database = event.target.result;
 
+    // check if app is online, run the loadTransaction() function
     if (navigator.onLine) {
-        uploadTransaction();
+        loadTransaction();
     }
 };
 
@@ -23,6 +23,7 @@ connect.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
+// Runs saveTransaction() function if a new transaction is submitted while user is offline
 function saveTransaction(record) {
     const transaction = database.transaction(['new_transaction'], 'readwrite');
     
@@ -32,14 +33,19 @@ function saveTransaction(record) {
 };
 
 function loadTransaction() {
+
+    // Open a transaction
     const transaction = database.transaction(['new_transaction'], 'readwrite');
-
+    
+    // access users object store
     const storeBudget = transaction.objectStore('new_transaction');
-
+    
+    // get all records from store
     const showAll = storeBudget.getAll();
 
     showAll.onsuccess = function () {
 
+        // Send any store data in indexedDb to api server
         if (showAll.result.length > 0) {
             fetch('/models/transaction', {
                 method: 'POST',
@@ -54,11 +60,14 @@ function loadTransaction() {
                 if (serverResponse.message) {
                     throw new Error(serverResponse);
                 }
-
+                
+                // open another transaction
                 const transaction = database.transaction(['new_transaction'], 'readwrite');
 
+                // access the new transaction stored
                 const storeBudget = transaction.objectStore('new_transaction');
 
+                // Clear the stored budget
                 storeBudget.clear();
 
                 alert('Saved transactions have been submitted');
@@ -69,3 +78,6 @@ function loadTransaction() {
         }
     }
 };
+
+// listen for app coming back online
+window.addEventListener('online', loadTransaction);
